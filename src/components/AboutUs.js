006068b9b41a1cb9bc1d9c0da72d92040e3fb0d5
Talha Khan <mail.talha.pro@gmail.com>
2021-06-05 04:35:25 +0500

    Text Updated

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -33,13 +33,7 @@ const AboutUs = () => {
             <p className='about__text'>
               Creditfix is a licensed lending platform that does data driven
               decision making and assessment for individual and small businesses
-              with speed, with the ability to entertain underserved and unserved
-              segments get a loan conveniently and quickly. Creditfix enables
-              users to get a loan when others would not serve, and at the best
-              rates for micro entrepreneurs, small businesses, private & public
-              sector partners, and unserved & underserved “missing middle”
-              consumers to help them get credit for specific use cases
-              especially assets, for themselves and networks.
+              with speed, with the ability to entertain.
             </p>
           </Col>
         </Row>
--- src/components/SupplyChain.js
+++ src/components/SupplyChain.js
@@ -1,47 +1,47 @@
-import React from "react"
-import SupplyCard from "./SupplyCard"
-import Line from "./Line"
-import { Container, Row, Col, Image } from "react-bootstrap"
-import supplier from "../images/supplier.svg"
-import customer from "../images/customer.svg"
-import merchant from "../images/merchant.svg"
-import backgroundPattern from "../images/backgroundPattern.svg"
+import React from 'react';
+import SupplyCard from './SupplyCard';
+import Line from './Line';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import supplier from '../images/supplier.svg';
+import customer from '../images/customer.svg';
+import merchant from '../images/merchant.svg';
+import backgroundPattern from '../images/backgroundPattern.svg';
 
 const SupplyChain = () => {
   return (
-    <div className="supply">
-      <Image fluid={true} className="background" src={backgroundPattern} />
+    <div className='supply'>
+      <Image fluid={true} className='background' src={backgroundPattern} />
       <Container>
         <Row>
-          <Col className="supply__upper-col">
-            <Line type="large" />
-            <div className="supply__heading">Supply Chain Financing</div>
+          <Col className='supply__upper-col'>
+            <Line type='large' />
+            <div className='supply__heading'>Supply Chain Financing</div>
           </Col>
         </Row>
       </Container>
       <Container>
         <Row>
-          <Col className="supply__lower-col">
+          <Col className='supply__lower-col'>
             <SupplyCard
               image={supplier}
-              title="Suppliers"
-              text="Easy credit to enhance capacity of your suppliers"
+              title='Suppliers'
+              text='Easy credit to enhance capacity of your suppliers'
             />
             <SupplyCard
               image={customer}
-              title="Customers"
-              text="Enhance your sales through easy customer financing"
+              title='Customers'
+              text='Enhance your sales through easy customer financing'
             />
             <SupplyCard
               image={merchant}
-              title="Merchants"
-              text="Enhancing purchasing capacity of merchant customers"
+              title='Merchants'
+              text='Enhancing purchasing capacity of merchants'
             />
           </Col>
         </Row>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default SupplyChain
+export default SupplyChain;
